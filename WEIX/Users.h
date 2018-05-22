c0f46ef43e910efc07d49683fda62c776a585b65
miasakachenmo <303306714@qq.com>
2018-05-22 23:47:51 +0800

    [A]BaseUser大致框架

---
+++ WEIX/Users.h
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<map>
+using namespace std;
+class Date
+{
+public:
+	static int MonthDay[12];
+	static int LeapMonthDay[12];
+	int Year;
+	int Month;
+	int Day;
+	void SetBirthday()
+	{
+		int *MonthDayLimit;
+		do
+		{
+			cout << "输入出生年份:"<<endl;
+			cin >> Year;
+			cout << "输入出生月份:"<<endl;
+			cin >> Month;
+			cout << "输入出生日:"<<endl;
+			cin >> Day;
+			if (Year % 100 != 0 && Year % 4 == 0)MonthDayLimit = LeapMonthDay;//年份是闰年
+			else MonthDayLimit = MonthDay;
+			if (!(Year > 0 && Year <= 2018 && Month > 0 && Month <= 12 && Day > 0 && Day <= MonthDayLimit[Month - 1]))
+			{
+				cout << "输入有误,请重新输入" << endl;
+				fflush(stdin);
+			}
+			else
+				return;
+		} while (!(Year > 0 && Year <= 2018 && Month > 0 && Month <= 12&&Day>0&&Day<=MonthDayLimit[Month-1]));
+	}
+};
+class BaseUser {
+public:
+	string Name;//昵称
+	string id;//分别到各个产品的号 比如QQ号
+	int ProductCode;//表示该用户存在的版本(此处主要用途是虚继承)
+	Date Birthday;
+
+	BaseUser()//创建模式  ,  在基类中不执行文件操作 
+	{
+
+		cout << "输入昵称";
+		cin >> Name;
+		Birthday.SetBirthday();
+	}
+	map<string, int> GlobalFriendMap;//全微X通用的好友列表
+	map<string, string> FriendMap;//单个应用的好友列表
+	map<string, int> GroupMap;//单个应用的群列表
+
+private:
+	string Global_id;//全局ID
+	Date SignDay;//注册日(用来计算X龄)
+};
---
+++ WEIX/main.cpp
@@ -0,0 +1,10 @@
+#include "Users.h"
+using namespace std;
+int Date::MonthDay[12] = { 31,27,31,30,31,30,31,31,30,31,30,31 };
+int Date::LeapMonthDay[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+int main()
+{
+	BaseUser a;
+	system("pause");
+	return 0;
+}
